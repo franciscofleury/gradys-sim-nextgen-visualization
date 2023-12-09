b7fe4949e36d76b48839bd788286ab8513b34e10
Thiago Lamenza <thlamenza@hotmail.com>
2023-12-08 22:45:59 -0300

    Adding feedback for connection

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,15 @@ import {InitializationData, SimulationData} from "./data";
 
 let socket = null;
 
+const panelElement = document.getElementById("panel");
+const connectionElement = document.getElementById("connection");
+function toggleUI(hide: boolean) {
+    connectionElement.style.visibility = hide ? "visible" : "hidden";
+    panelElement.style.visibility = !hide ? "visible" : "hidden";
+}
+
+toggleUI(true);
+
 function socketConnectionFailed(
     initCallback: (data: InitializationData) => void,
     onDataCallback: (data: SimulationData) => void,
@@ -24,6 +33,7 @@ export function connectToSocket(
     socket = new WebSocket("ws://localhost:5678");
     socket.onopen = function (e) {
         console.log("[open] Connection established");
+        toggleUI(false)
     };
 
     socket.onmessage = (event) => {
@@ -43,7 +53,10 @@ export function connectToSocket(
             console.log('[close] Connection died');
         }
         onFinalizeCallback()
+        firstMessage = false;
+        toggleUI(true);
         socketConnectionFailed(initCallback, onDataCallback, onFinalizeCallback)
+
     };
 
     socket.onerror = function (error) {
